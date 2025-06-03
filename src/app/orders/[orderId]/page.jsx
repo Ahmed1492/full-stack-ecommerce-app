d1f234e2ee902421ba4d2baee08c5f323d2607de
Ahmed Mohamed <rmmhnrnrjr@gmail.com>
2025-06-03 03:40:51 +0300

    Handle User Orders , Profile

--- src/app/orders/[orderId]/page.jsx
+++ src/app/orders/[orderId]/page.jsx
@@ -1,26 +1,41 @@
+import axios from "axios";
 import React from "react";
 
-export default function page() {
+export default async function page({ params }) {
+  const { orderId } = params;
+  console.log("orderId ", orderId);
+
+  let getOrder = async () => {
+    try {
+      let myResponse = await axios.get(
+        `http://localhost:2000/orders/${orderId}`
+      );
+      console.log(myResponse.data.order);
+
+      return myResponse.data.order;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  let orderDetails = await getOrder();
   return (
     <div className="flex flex-col gap-4 mt-[4rem] items-center">
       <h3 className="text-center font-semibold text-2xl">Order Detaisl</h3>
       {/* Order Id */}
       <div className="flex flex-col gap-4 text-left justify-start  text-xl font-light   mt-[2rem]   ">
-        <span>Order Id :1232132k1lj132lk12lldsql</span>
+        <span>Order Id :{orderDetails?._id}</span>
         {/* Receiver name */}
-        <span>Receiver name :ahmed mohamed</span>
+        <span>Receiver name :{orderDetails.receiverName}</span>
         {/* Receiver Email */}
-        <span>Receiver Email :ahmed@gmail.com</span>
+        <span>Receiver Email :{orderDetails.receiverEmail}</span>
         {/* price */}
-        <span>price : 0.0</span>
+        <span>price : {orderDetails.price}</span>
         {/* Payment Status */}
-        <span> Payment Status :PAID</span>
+        <span> Payment Status :{orderDetails.paymentStatus}</span>
         {/* Order Status */}
-        <span> Order Status :APPROVED</span>
+        <span> Order Status :{orderDetails.orderStatus}</span>
         {/* Delivery Address */}
-        <span>
-          Delivery Address :Ms. Susan Holmes, 4 Princeton, Little Whinging,
-        </span>
+        <span>Delivery Address :{orderDetails.address}</span>
 
         <span
           className="border-b border-black text-lg pb-0 text-gray-800 font-semibold cursor-pointer
--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-
-export default function OrderHistory() {
+import axios from "axios";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Link from "next/link";
+export default async function OrderHistory() {
   const allHistoryOrders = [
     {
       orderId: "sgfa231....",
@@ -27,24 +30,48 @@ export default function OrderHistory() {
       status: "APPROVED",
     },
   ];
+  dayjs.extend(relativeTime);
+
+  let userId = "a9d38b31-e2d0-4884-aecc-8d12e1c6c838";
+
+  const getUserOrders = async () => {
+    try {
+      let myResponse = await axios.get(
+        `http://localhost:2000/userOrders/${userId}`
+      );
+      // console.log("====================================");
+      // console.log(myResponse.data.result);
+      // console.log("====================================");
+      return myResponse?.data?.result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // getUserOrders();
+  let allOrders = await getUserOrders();
+  // console.log(" allOrders", allOrders);
 
   return (
     <div className="">
       <h2 className="text-2xl mb-[2rem] font-semibold ">Orders</h2>
       {/* ALL ORDERS */}
       <div className="flex flex-col gap-[1rem]">
-        {allHistoryOrders.map((order, index) => (
-          <div
+        {allOrders?.map((order, index) => (
+          <Link
+            href={`/orders/${order._id}`}
             key={index}
             className={`cursor-pointer ${
               (index + 1) % 2 === 0 ? "bg-slate-100" : ""
             }  px-4 py-5 rounded-md   text-lg flex items-center gap-[3rem]`}
           >
-            <span>ksadls....</span>
-            <span>0.00$</span>
-            <span>1 hour ago</span>
-            <span>APPROVED</span>
-          </div>
+            <span>
+              {order._id.slice(0, 4)}...{order._id.slice(-4)}
+            </span>
+
+            <span>${order.price}</span>
+            <span>{dayjs(order.createdAt).fromNow()}</span>
+            <span>{order.paymentStatus}</span>
+          </Link>
         ))}
       </div>
     </div>
